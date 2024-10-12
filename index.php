b16ad1a16d71477704c0583a1596ae6a2194bcfc
Jhonny Ribeiro <jaribeiro32@gmail.com>
2024-10-12 16:16:33 -0300

    nav bar

--- index.php
+++ index.php
@@ -8,7 +8,22 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
+<body class="bg-stone-950 text-stone-200">
+
+    <header class="bg-stone-900">
+
+        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+            <div class="font-bold text-xl tracking-wide">BookWise</div>
+            <ul class="flex space-x-4 font-bold">
+                <li><a href="/" class="text-cyan-600">Explorar</a></li>
+                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="/login.php" class="hover:underline">Login</a></li>
+            </ul>
+        </nav>
+    </header>
 
 </body>
 
